3cc93f6791a7580321e5c6c6bd56e76fecabe58d
John Carlo Salter <juuncaerlum@gmail.com>
2019-01-19 14:05:31 -0600

    Adding function to update description file.

--- gkr/R/make-branch.R
+++ gkr/R/make-branch.R
@@ -12,3 +12,26 @@ make_branch <- function(user, repository, branch_name, sha) {
     sha = sha
   )
 }
+
+# PUT /repos/:owner/:repo/contents/:path
+update_description_file <- function(
+  repository_owner,
+  repository,
+  path = './DESCRIPTION',
+  commit_message,
+  b64_content,
+  file_sha,
+  branch
+  ) {
+  endpoint = "PUT /repos/:owner/:repo/contents/:path"
+  gh(
+    endpoint,
+    owner = repository_owner,
+    repo = repository,
+    path = path,
+    message = commit_message,
+    content = b64_content,
+    sha = file_sha,
+    branch = branch
+  )
+}
