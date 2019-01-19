a87b207bd9cc46c6141a0044be53643f5c046cc1
Devin Pastoor <devin.pastoor@gmail.com>
2019-01-19 15:27:34 -0600

    test: full dep equality

--- gkr/tests/testthat/test-new-descriptions.R
+++ gkr/tests/testthat/test-new-descriptions.R
@@ -4,7 +4,11 @@ describe("new description can be created", {
   desc_text <- read_test_file("DESCRIPTION_logrrr")
   it("can replace a single package", {
     dep_desc <- pkg_deps(desc_text)
-    expect_equal(dep_desc$deps$package, c("R6", "crayon", "glue", "rlang", "testthat", "jsonlite", "covr",
-                                          "sessioninfo"))
+    expected <- structure(list(type = c("Imports", "Imports", "Imports", "Imports",
+                                        "Suggests", "Suggests", "Suggests", "Suggests"), package = c("R6",
+                                                                                                     "crayon", "glue", "rlang", "testthat", "jsonlite", "covr", "sessioninfo"
+                                        ), version = c("*", "*", ">= 1.3.0", ">= 0.2.1", "*", "*", "*",
+                                                       "*")), row.names = c(NA, -8L), class = "data.frame")
+    expect_equal(dep_desc$deps, expected)
   })
 })
