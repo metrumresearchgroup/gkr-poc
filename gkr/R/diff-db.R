ede72684df7b6099347c59431817ff8e6931f63f
Devin Pastoor <devin.pastoor@gmail.com>
2019-01-19 11:37:37 -0600

    feat: start implementing api

---
+++ gkr/R/diff-db.R
@@ -0,0 +1,7 @@
+#' diff the packages versions available in two package databases
+#' @param .ndb new database
+#' @param .odb old database
+#' @export
+diff_pkgdb <- function(.ndb, .odb) {
+  TODO("diff db")
+}
